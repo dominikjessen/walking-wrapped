4b73001db8db15c9cc9b8b038f2d942b7f0936d1
Dominik <djessen94@gmail.com>
2024-09-02 00:32:13 +0200

    Some login page styling

--- src/app/(not-logged-in)/magic-link/page.tsx
+++ src/app/(not-logged-in)/magic-link/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import { SubmitButton } from '@/components/ui/SubmitButton';
@@ -24,35 +23,15 @@ export default function MagicLink({ searchParams }: { searchParams: { message: s
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center items-center gap-2">
-      <Link
-        href="/"
-        className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
-
-      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+    <div className="flex flex-col w-full sm:max-w-md justify-center items-center gap-2 m-auto h-screen">
+      <form className="flex flex-col w-full justify-center gap-2 text-foreground bg-white p-10 rounded-xl">
+        <h1 className="text-xl font-bold mb-4">Sign in with a magic link</h1>
         <label className="text-md" htmlFor="email">
           Email
         </label>
         <input className="rounded-md px-4 py-2 bg-inherit border mb-6" name="email" placeholder="you@example.com" required />
 
-        <SubmitButton formAction={signInWithMagicLink} className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2" pendingText="Signing In...">
+        <SubmitButton formAction={signInWithMagicLink} className="bg-teal-400  rounded-md px-4 py-2 text-foreground mb-2" pendingText="Signing In...">
           Sign In
         </SubmitButton>
 
