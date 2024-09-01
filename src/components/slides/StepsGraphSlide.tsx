1ff418da68f4ce706da684c61b99f85e36709e81
Dominik <djessen94@gmail.com>
2024-09-02 00:13:08 +0200

    Combine average and graph slides

--- src/components/slides/StepsGraphSlide.tsx
+++ src/components/slides/StepsGraphSlide.tsx
@@ -1,17 +1,46 @@
 import { motion } from 'framer-motion';
 import StepsPerDayGraph from '@/components/graphs/StepsPerDayGraph';
+import { useUserStepsStore } from '@/stores/userStepsStore';
+import { calculateDogStepPercentile, calculateHumanStepPercentile } from '@/lib/steps';
+import { formatNumber } from '@/lib/utils';
 
 export default function AverageStepsSlide() {
+  const { steps } = useUserStepsStore();
+
+  const totalSteps = steps.reduce((acc, entry) => acc + entry.steps, 0);
+  const averageSteps = totalSteps / steps.length;
+
+  const humanPercentile = calculateHumanStepPercentile(averageSteps);
+  const dogPercentile = calculateDogStepPercentile(averageSteps);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-500 to-blue-500"
+      className="flex flex-col gap-8 items-center justify-center bg-teal-100 h-screen overflow-hidden w-screen px-8"
     >
-      <h1 className="text-4xl font-bold text-white">Slide 3 - Graph steps</h1>
-      <p className="text-lg text-white mt-4">This is what you have been up to:</p>
+      <h2>You want the day-by-day play-by-play? Here you go:</h2>
+
       <StepsPerDayGraph />
+
+      <div className="flex flex-col gap-4 pt-4">
+        <h2>
+          That's an average of <span className="font-bold">{formatNumber(averageSteps, 'standard', 0)}</span> every single day!
+        </h2>
+        <div className="flex gap-2 items-center">
+          <div>
+            Which puts you in the <span className="font-bold">{humanPercentile}th</span> percentile of all humans...
+          </div>
+          <div className="text-4xl">🚶</div>
+        </div>
+        <div className="flex gap-2 items-center justify-between">
+          <div>
+            ...and the <span className="font-bold">{dogPercentile}th</span> percentile for dogs.
+          </div>
+          <div className="text-4xl">🐕</div>
+        </div>
+      </div>
     </motion.div>
   );
 }
