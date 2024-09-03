e2bb3592d2d7c6c2d7bd4c2cc313069bc6c9035b
Dominik <djessen94@gmail.com>
2024-09-04 00:16:31 +0200

    Reveal animations for StepsGraphSlide

--- src/components/slides/StepsGraphSlide.tsx
+++ src/components/slides/StepsGraphSlide.tsx
@@ -3,6 +3,8 @@ import StepsPerDayGraph from '@/components/graphs/StepsPerDayGraph';
 import { useUserStepsStore } from '@/stores/userStepsStore';
 import { calculateDogStepPercentile, calculateHumanStepPercentile } from '@/lib/steps';
 import { formatNumber } from '@/lib/utils';
+import { TEXT_REVEAL_ANIMATION_DURATION } from '@/lib/constants';
+import { useState, useEffect } from 'react';
 
 export default function AverageStepsSlide() {
   const { steps } = useUserStepsStore();
@@ -13,6 +15,28 @@ export default function AverageStepsSlide() {
   const humanPercentile = calculateHumanStepPercentile(averageSteps);
   const dogPercentile = calculateDogStepPercentile(averageSteps);
 
+  // Animation vs content
+  // TODO: Add animation
+  const [showContent, setShowContent] = useState(true);
+
+  // Staggered content reveal
+  const [showTitle, setShowTitle] = useState(false);
+  const [showGraph, setShowGraph] = useState(false);
+  const [showAverage, setShowAverage] = useState(false);
+  const [showHuman, setShowHuman] = useState(false);
+  const [showDog, setShowDog] = useState(false);
+
+  // Animation control
+  useEffect(() => {
+    // setTimeout(() => setShowContent(true), TEXT_REVEAL_ANIMATION_DURATION * 3);
+    setTimeout(() => setShowTitle(true), TEXT_REVEAL_ANIMATION_DURATION * 0);
+    setTimeout(() => setShowGraph(true), TEXT_REVEAL_ANIMATION_DURATION * 0.75);
+    setTimeout(() => setShowAverage(true), TEXT_REVEAL_ANIMATION_DURATION * 2);
+
+    setTimeout(() => setShowHuman(true), TEXT_REVEAL_ANIMATION_DURATION * 3);
+    setTimeout(() => setShowDog(true), TEXT_REVEAL_ANIMATION_DURATION * 3.75);
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -20,26 +44,48 @@ export default function AverageStepsSlide() {
       exit={{ opacity: 0 }}
       className="flex flex-col gap-8 items-center justify-center bg-teal-100 h-screen overflow-hidden w-screen px-8"
     >
-      <h2>You want the day-by-day for your steps? Coming right up.</h2>
+      {showTitle ? (
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full">
+          <div className="text-center">You want the day-by-day of your steps? Coming right up.</div>
+        </motion.div>
+      ) : null}
 
-      <StepsPerDayGraph />
+      {showGraph ? (
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full">
+          <StepsPerDayGraph />
+        </motion.div>
+      ) : null}
 
-      <div className="flex flex-col gap-4 pt-4">
-        <h2>
-          That's an average of <span className="font-bold">{formatNumber(averageSteps, 'standard', 0)}</span> every single day!
-        </h2>
-        <div className="flex gap-2 items-center">
-          <div>
-            Did you know this puts you in the <span className="font-bold">{humanPercentile}th</span> percentile of all humans...
-          </div>
-          <div className="text-4xl">🚶</div>
-        </div>
-        <div className="flex gap-2 items-center justify-between">
-          <div>
-            ...but only the <span className="font-bold">{dogPercentile}th</span> percentile for dogs.
-          </div>
-          <div className="text-4xl">🐕</div>
-        </div>
+      <div className="flex flex-col gap-4">
+        {showAverage ? (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full">
+            <div className="text-center mb-4">
+              That's an average of <span className="font-bold">{formatNumber(averageSteps, 'standard', 0)}</span> every single day!
+            </div>
+          </motion.div>
+        ) : null}
+
+        {showHuman ? (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full">
+            <div className="flex gap-2 items-center justify-between text-lg">
+              <div>
+                Did you know this puts you in the <span className="font-bold">{humanPercentile}th</span> percentile of all humans...
+              </div>
+              <div className="text-4xl">🚶</div>
+            </div>
+          </motion.div>
+        ) : null}
+
+        {showDog ? (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="w-full">
+            <div className="flex gap-2 items-center justify-between text-lg">
+              <div>
+                ...but only the <span className="font-bold">{dogPercentile}th</span> percentile for dogs.
+              </div>
+              <div className="text-4xl">🐕</div>
+            </div>
+          </motion.div>
+        ) : null}
       </div>
     </motion.div>
   );
