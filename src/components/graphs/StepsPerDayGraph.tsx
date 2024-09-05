a0fbd902f5f2a3b9af3cccc4791ea545e0f26ff2
Dominik <djessen94@gmail.com>
2024-09-05 07:57:45 +0200

    Better looking graph

--- src/components/graphs/StepsPerDayGraph.tsx
+++ src/components/graphs/StepsPerDayGraph.tsx
@@ -10,7 +10,7 @@ import { ComponentProps } from 'react';
 const chartConfig = {
   desktop: {
     label: 'Desktop',
-    color: '#2563eb'
+    color: '#588157'
   }
 } satisfies ChartConfig;
 
@@ -18,13 +18,13 @@ export default function StepsPerDayGraph({ className }: ComponentProps<'div'>) {
   const { steps } = useUserStepsStore();
 
   return (
-    <ChartContainer config={chartConfig} className={cn('min-h-[256px] w-full px-1 py-2 lg:py-4 bg-white rounded-xl', className)}>
+    <ChartContainer config={chartConfig} className={cn('min-h-[256px] w-full pr-2 py-2 lg:py-4 bg-white rounded-xl', className)}>
       <LineChart accessibilityLayer data={steps}>
         <CartesianGrid vertical={false} />
-        <XAxis dataKey="date" tickFormatter={(value) => formatDate(value)} />
-        <ChartTooltip content={<ChartTooltipContent />} />
-        <YAxis tickFormatter={(value) => formatNumber(value, 'compact', 0)} />
-        <Line type="monotone" dataKey="steps" stroke="#8884d8" />
+        <XAxis dataKey="date" tickMargin={4} tickFormatter={(value) => formatDate(value)} />
+        <ChartTooltip content={<ChartTooltipContent labelFormatter={(label) => formatDate(label)} />} />
+        <YAxis width={32} tickFormatter={(value) => formatNumber(value, 'compact', 0)} axisLine={false} tickLine={false} tickMargin={4} />
+        <Line type="monotone" dataKey="steps" stroke="var(--color-desktop)" strokeWidth={2} dot={false} />
       </LineChart>
     </ChartContainer>
   );
